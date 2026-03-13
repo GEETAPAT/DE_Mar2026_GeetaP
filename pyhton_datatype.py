74394aaa0508d6c818474051e04fc4fdade65350
GEETAPAT <37790317+GEETAPAT@users.noreply.github.com>
2026-03-13 13:22:40 +0000

    Practice on DataType

---
+++ pyhton_datatype.py
@@ -0,0 +1,7 @@
+#integer data type
+
+# 1. Adding new students to the total
+new_students_batch1 = 120
+total_students = 1500
+total_students = total_students + new_students_batch1
+print(f"1. Total students after batch 1: {total_students}") # Output: 1620
